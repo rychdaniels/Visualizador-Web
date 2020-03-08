39890b7c32e92e39398ce8d324ad782607fca7b1
rych <rychraya@gmail.com>
2020-03-08 11:20:49 -0600

    Se agrego informacion sobre el funcionamiento de los metodos, se integro el funcionamiento para cambiar el color de la trayectoria de la particula al llegar a cierto punto(Aun no funciona, no cambia de color al llegar al punto)

--- JS/particulas.js
+++ JS/particulas.js
@@ -6,14 +6,18 @@ function Particulas(visualizador,json) {
     this.funciones = 0;
     this.pars = [];//particulas (objetos)
     this.color = [];//colores
-    this.trays = [];//posiciones
+    this.trays = [];//posiciones de cada particula
     this.trayso = [];//lineas
-    this.colorTrayectoria = [];
-    this.play = false;
-    this.aislar = false;
-    this.numeroParticula = null;   
+    this.colorTrayectoria = []; //rreglo para almacenar el color de la trayectoria de una particula aislada
+    this.play = false; //Para detener o reanudar la simulacion
+    this.aislar = false; //Guarda si la particula es sera aislada
+    this.numeroParticula = null; //Numero de particula que ha sido aislada
 
-	
+	/**
+     * Metodo encargado de crear el menu para cada una de las particulas,
+     * se crea un menu por cada visualizador, cada menu tiene un 
+     * identificador que corresponde a cada visualizador
+     */
     this.mostrarMenu = function () {
         var contenedor= "<div class='row' id='visualizador"+visualizador.id+"'"+">"+
                         " <div class='container col-sm-10' id='"+visualizador.id+"'"+"></div> " +
@@ -72,6 +76,11 @@ function Particulas(visualizador,json) {
     this.draw = function() {
         var objParticulas=mySelf;
         
+        /**
+         * Metodo encargado de crear el lugar donde se visualizara la simulacion, este metodo crea
+         * -> La escena
+         * -> Las paredes del lugar de la visualizacion
+         */
         this.dibujaCanal = function () {            
             
             var elemento = document.getElementById(visualizador.id.toString());   
@@ -177,32 +186,42 @@ function Particulas(visualizador,json) {
 
         }
 
-
+        /**
+         * Metodo encargado de dibujar cada una de las particulas
+         */
         this.dibujaParticulas = function ( ) {
             this.dibujaCanal();
-            
+            //Generamos un color pseudoaleatorio
             var color = 1 + this.numeroParticula * 100;            
+            //Nos aseguramos que el objeto este vacio
             objParticulas.particulas = {};
+            //Si la particula es aislada
             if ( this.aislar ) {
                 
+                //Leemos las propeidades de archivo JSON
                 objParticulas.particulas = json.particles.particle[this.numeroParticula];
-                                
-                    var x = objParticulas.particulas.pasos[0].x;
-                    var y = objParticulas.particulas.pasos[0].y;     
-                         
-                    var p = new THREE.SphereGeometry(.01, 10, 10); //(radio, ..., ...)
-                    var aux = color * 111111;
-                    objParticulas.color.push(aux);
-                    var material = new THREE.MeshBasicMaterial({ color: aux });
-                    var sphere = new THREE.Mesh(p, material);
-                    sphere.position.x = parseFloat(x);
-                    sphere.position.y = parseFloat(y);                
-                    visualizador.scene.add(sphere);
-                    objParticulas.pars.push(sphere);    
-                    objParticulas.trays.push([{ "x": x, "y": y }]);//se guarda pos para las trayectorias
+                //Obtenemos solo los pasos de una particula
+                var x = objParticulas.particulas.pasos[0].x;
+                var y = objParticulas.particulas.pasos[0].y;     
+                //Creamos las esferas
+                var p = new THREE.SphereGeometry(.01, 10, 10); //(radio, ..., ...)
+                var aux = color * 111111; //Generamos el color
+                objParticulas.color.push(aux);//Guardamos el color
+                //Asignamos el color a la particula
+                var material = new THREE.MeshBasicMaterial({ color: aux });
+                //Creamos la esfera y le asignamos su tamaño y color
+                var sphere = new THREE.Mesh(p, material);
+                //Asignamos las posiciones para la esfera
+                sphere.position.x = parseFloat(x);
+                sphere.position.y = parseFloat(y);       
+                //Agregamos la esfera al visualizador         
+                visualizador.scene.add(sphere);
+                objParticulas.pars.push(sphere);    
+                //Guardamos las trayectorias de la esfera
+                objParticulas.trays.push([{ "x": x, "y": y }]);//se guarda pos para las trayectorias
                     
 
-            } else {
+            } else { //Si la particula no es aislada
                 objParticulas.particulas = json.particles.particle;
                 
                 //dibuja particulas y las coloca en la primer posicion
@@ -231,34 +250,46 @@ function Particulas(visualizador,json) {
         this.dibujaParticulas();
     }
 
+    /**
+     * Metodo encargado de guardar cada una de la posiciones de las
+     * particulas
+     * @params
+     * aislar : Indica si la particula sera aislada
+     * objParticulas: Es el objeto que contiene toda la informacion de
+     *                la particula.
+     */
     this.setPos = function ( aislar = false, objParticulas) {
         
-        
+        //Si la particula NO es aislada
         if ( aislar == false ) {
             var pasoID = "pos"+visualizador.id;
             var checkID = "Checkpt1"+visualizador.id;
             
+            //Recorremos el arreglo de cada una de las particulas
             for (var i = 0; i < this.particulas.length; i++) {
                 if (this.paso < this.particulas[i].pasos.length) {
+                    //Guardamos cada posicion
                     var x = parseFloat(this.particulas[i].pasos[this.paso].x);
                     var y = parseFloat(this.particulas[i].pasos[this.paso].y);
+                    //Metemos cada posicion en el arreglo pars 
                     this.pars[i].position.setX(x);                    
                     this.pars[i].position.setY(y);
+                    //Guardamos en el arreglo trays cada uno de los puntos
                     this.trays[i].push({ "x": x, "y": y });
                     
                 }             
                 
                 
             }
+            //Se muestran cada uno de los pasos recorridos en el navegador
             document.getElementById(pasoID).innerHTML = this.paso;
             checkbox = document.getElementById(checkID);        
             
+            //Si Trayectoria esta marcado, se mostraran las traqyectorias de las particulas
             if (checkbox.checked == true) {
                 this.muestraTray(objParticulas);
             }
-        } else {            
-
-            
+        } else {//La particula es aislada, repetimos el procedimiento anterior pero solo para una particula
             if (this.paso < this.particulas.pasos.length) {   
                 var x = parseFloat(this.particulas.pasos[this.paso].x);
                 var y = parseFloat(this.particulas.pasos[this.paso].y);                                
@@ -278,53 +309,76 @@ function Particulas(visualizador,json) {
         // console.log('Desde setPos, aislar = ' + this.aislar + "Visualizador " + visualizador.id);
     }
 
-    
+    /**
+     * Metodo encargado de mostrar las trayectoria de cada una de las
+     * particulas, funciona para particula aislada y para todo el arreglo
+     * de particulas
+     */
 
-    this.muestraTray = function (objParticulas) {
+    this.muestraTray = function () {
         
         this.trayso.forEach(function (tray) {
             visualizador.scene.remove(tray);
         });
         this.trayso = [];
-        var vertices = [];
-        var color = new THREE.Color();
-        let xP = 300;
+        var vertices = [];//Puntos que seran unidos para generar la linea de la trayectoria
+        
+        let xP = 1000;//Punto en el que la particula cambiara de color su trayectoria
         // console.log(this.aislar);
         if (checkbox.checked == true) {
             for (var i = 0; i < this.trays.length; i++) {    
                 
-                var geometry = new THREE.BufferGeometry();
-                
-                
-                for (var j = 0; j < this.trays[i].length; j++) {
-                    var x = this.trays[i][j].x;
-                    var y = this.trays[i][j].y;
-                    vertices.push(x, y, 0);
+                if(this.trays.length==1){//Si la particula esta aislada
+                    var geometry = new THREE.BufferGeometry();
                     
-                    color.setHSL( 0.6, 1.0, Math.max( 0, -x / 200 ) + 0.5 );
-                    this.colorTrayectoria.push(color.r, color.g, color.b);
-                    
-                    
-                    // geometry.vertices.push(new THREE.Vector3(x, y, 0));
-                    
-                }
-                geometry.addAttribute( 'position', new THREE.Float32BufferAttribute( vertices, 3 ) );
-                geometry.addAttribute( 'color', new THREE.Float32BufferAttribute( this.colorTrayectoria, 3 ) );
-                var material = new THREE.LineBasicMaterial( {color: 0xffffff, vertexColors: THREE.VertexColors} );
+                    var colorLinea = [];
+                    for (var j = 0; j < this.trays[i].length; j++) {
+                        var x = this.trays[i][j].x;
+                        var y = this.trays[i][j].y;
+                        vertices.push( x, y, 0);
+                        if(i < xP){
+                            // Color de la linea: Rojo
+                            colorLinea.push( 255, 0, 0 );
+                        } else{
+                            // Color de la linea: Blanco
+                            colorLinea.push(51, 255, 85 );
 
-                var tray = new THREE.Line(geometry, material);
-                visualizador.scene.add(tray);
-                this.trayso.push(tray);
-                
+                        }
+                               
+                    } 
+
+                    //Pasamos las posiciones
+                    geometry.addAttribute( 'position', new THREE.Float32BufferAttribute( vertices, 3 ) );
+                    // Pasamos el color
+                    geometry.addAttribute( 'color', new THREE.Float32BufferAttribute( colorLinea, 3 ) );
+
+                    var material = new THREE.LineBasicMaterial( { color: 0xffffff, vertexColors: THREE.VertexColors } );                                           
+                    var tray = new THREE.Line(geometry, material);
+                    visualizador.scene.add(tray);
+                  
+
+                }else{ //Si la particula no esta aislada
+                    
+                    var geometry = new THREE.Geometry();
+
+                    var material = new THREE.LineBasicMaterial( {color:this.color[i]} );
+                    for (var j = 0; j < this.trays[i].length; j++) {
+                        var x = this.trays[i][j].x;
+                        var y = this.trays[i][j].y;
+                        geometry.vertices.push(new THREE.Vector3(x, y, 0));
+
+                    }
+                     var tray = new THREE.Line(geometry, material);
+                    visualizador.scene.add(tray);
+                    this.trayso.push(tray);
+                }
 
             }
         }
 
     }
 
-
-    
-
+    // Metodo para pausar la simulacion
     this.pause = function () {
         // console.log('click de: ' + visualizador.id);
         if (this.play == true) {
@@ -355,14 +409,20 @@ function Particulas(visualizador,json) {
     }      
     
     
-    
+    /**
+     * Metodo que crea un visulizador nuevo y ademas un menu cuando
+     * la particula se aisla
+     */
     this.aislaParticula = function (particula, nuevoVisualizador){      
         
+        //Si la particula no existe en el arreglo
         if( particula >= this.particulas.length ){
+            //Mensaje de error
             const mensaje = "<div id = 'mensaje'>"+
                                 "<h3>Valor inválido</h3>"
                             "</div>";
-            
+            //Al presionar el boton aceptar aparecera el mensaje de error
+            //y desaperecera despues de 1.5 seg.
             $('#aceptar').after(mensaje); 
             setTimeout( () => {
                 $('#mensaje').remove();   
@@ -371,54 +431,47 @@ function Particulas(visualizador,json) {
             
             return false;
         }
-        this.colorTrayectoria=[];    
+        this.colorTrayectoria=[];
+        //bandera=true para poder observar la visualizacion    
         nuevoVisualizador.bandera = true;
-        
 
+        //Se crea el menu de la particula
         var nuevoItem = "<div class='nuevo' id ='particula"+nuevoVisualizador.id+"'>"+
                             "<div class='row'>"+
                                 "<button class='btn btn-block btn-info btn-titulo col-sm-8' disabled>Particula " + particula + "</button>" +
                                 "<button class='btn btn-danger btn-titulo col-sm-4' id='quitar"+nuevoVisualizador.id+"'> Quitar </button>" +
                             "</div>"           
-                        "</div>";         
-        object =  eval("new " + json.name + "(nuevoVisualizador,json)");       
+                        "</div>";        
+        //Se crea el objeto en es te caso de tipo Particula
+        object =  eval("new " + json.name + "(nuevoVisualizador,json)");
+        //Se indica que la particula sera aislada
         object.aislar = true;
+        //Guardamos el numero de la particula que ha sido aislada
         object.numeroParticula = particula;
+        //Pasamos el metodo draw() el archivo JSON que contiene la informacion de la particula aislada
         object.draw(json);     
         
         // console.log('nuevoVisualizador' + nuevoVisualizador.id);
+        //Agregamos el nuevo visualizador al nevagdor
         $('#visualizador'+nuevoVisualizador.id).before(nuevoItem); 
         $('#aislaParticula'+nuevoVisualizador.id).remove();
         
+        //Con ayuda de Jquery capturamos el evento cuando el usuario desee elminar el visualizador
+        //del navegador
         $('#quitar'+nuevoVisualizador.id).click(function(){
             // console.log('click');
             var respuesta = confirm("Desea eliminar el visualizador de particula #" + particula);
+            //Si se recibe una respuesta
             if( respuesta ) {
                 object = {};
                 $('#particula'+nuevoVisualizador.id).remove();
                 $('#visualizador'+nuevoVisualizador.id).remove();
-            } else {
+            } else {//Si el usuario cancela sólo retornamos false
                 return false;
             }
             
         })  
     }
-    
-    // this.buscaPosicion = function (objParticulas){
-    //     let xP = 1000;
-    //     console.log(this.particulas);
-    //     for (var i = 0; i < this.particulas.length; i++) {
-    //         if (this.paso < this.particulas[i].pasos.length) {
-
-    //             if ( xP == this.particulas[i].pasos[this.paso].x) {
-                    
-                
-    //                 objParticulas.color[i] = 00000;
-                    
-    //             }           
-    //         }    
-    //     }        
-    // }
     
      
     //EvenListeners: Se usa Jquery para capturar los eventos
@@ -436,7 +489,7 @@ function Particulas(visualizador,json) {
         $('#avanzar'+visualizador.id).click(function () {
             mySelf.avanzar();
         }),
-
+        //Si el checkbox esta marcado muestra las trayectorias
         $('#Checkpt1'+visualizador.id).change(function(){
             if($(this).is(":checked")){
                 mySelf.muestraTray();
@@ -444,22 +497,19 @@ function Particulas(visualizador,json) {
         }),        
 
       
-
         $('#aceptar').click(function(e){
             
-            var valor = $('#particula').val();            
+            var valor = $('#particula').val();
+            //Si el input es vacio
             if( valor == ''){   
-                              
-                e.preventDefault();
-            } else {         
+                e.preventDefault();//No mandamos nada
+            } else {//En otro caso
+                //Creasmo la nueva particula
                 mySelf.aislaParticula(valor, new Visualizador());
-                $('#particula').val('');                 
+                $('#particula').val('');//Limpiamos el campo para poder ingresar otra particula     
             }
         }),
-
-        
     );
-
 }
 
 
@@ -486,4 +536,3 @@ Particulas.prototype.animate = function (visualizador, objParticulas) {
 function texttoFunction(funcion) {
     return Parser.parse(funcion).toJSFunction(['x']);
 }
-
